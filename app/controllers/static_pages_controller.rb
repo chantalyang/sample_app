0e504180565127adc121c8edfd563e4c4b40e3ce
Chantal Yang <chantalyang17@gmail.com>
2013-11-26 10:16:38 +0200

    Added a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+#Static Page Class inherits from ApplicationController
+class StaticPagesController < ApplicationController
+  
+  #Home Method to render the view for Home
+  def home
+
+  end
+
+  #Help Method to render the view for Help
+  def help
+  end
+end
