6f8411e0277c2976078ea60e8ad439497529c6a0
Chantal Yang <chantalyang17@gmail.com>
2013-11-28 12:05:13 +0200

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
+#Contains methods to render pages associated with users
 class UsersController < ApplicationController
-  def new
+  
+  #Method to render View User Page  (Show)	
+  def show
+  	@user = User.find(params[:id])
   end
+
+
+  #Method to render New User Page
+  def new
+  	@user = User.new
+  end
+
+  #Method to render create page
+  def create
+    @user = User.new(user_params) #Call the user params method to fetch params   
+    if @user.save #If save successful
+      flash[:success] = "Welcome to the Sample App!" #Display welcome to the sample app
+      redirect_to @user #Redirect to the user show page
+    else #Else
+      render 'new' #Render new page 
+    end
+  end
+
+  private #Makes method only available to current class
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Chantal Yang"
+    email    "chantalyang17@gmail.com"
+    password "1a2b3c"
+    password_confirmation "1a2b3c"
+  end
+end
