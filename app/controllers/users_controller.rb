e5739d54fab15040fa368b73d2f39146d0249729
Chantal Yang <chantalyang17@gmail.com>
2013-11-29 14:45:59 +0200

    Sign In and Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 #Contains methods to render pages associated with users
 class UsersController < ApplicationController
+   before_action :signed_in_user, only: [:edit, :update]
+   #Check for singned_in user before edit and update actions
   
   #Method to render View User Page  (Show)	
   def show
   	@user = User.find(params[:id])
   end
 
+  #Method to render the edit page
+  def edit
+    @user = User.find(params[:id])
+  end
 
   #Method to render New User Page
   def new
   	@user = User.new
+  end
+
+  #Method to render edit page   
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   #Method to render create page
@@ -29,4 +47,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+#Render signed in user?
+def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
 end
